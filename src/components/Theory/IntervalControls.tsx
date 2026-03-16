af29096b788d2565b430d128be1dbd12f2802ef1
DamiDevApp <damgonsal13@gmail.com>
2026-03-16 14:09:30 -0600

    Modified Interval component
    Following module css structure

--- src/components/Theory/IntervalControls.tsx
+++ src/components/Theory/IntervalControls.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { INTERVALS } from "../../constants/intervals.constants";
-import { UI_COLORS } from "../../constants/theme.constants";
+import styles from "./IntervalControls.module.css";
 
 interface Props {
   root: string;
@@ -18,37 +18,19 @@ export const IntervalControls: React.FC<Props> = ({
   )?.label;
 
   return (
-    <div style={{ marginTop: 20, textAlign: "center" }}>
+    <div className={styles.container}>
       <h3>Interval Explorer</h3>
       <p>
         Root: <strong>{root}</strong> + Interval:{" "}
         <strong>{currentLabel}</strong>
       </p>
 
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: 5,
-          justifyContent: "center",
-          maxWidth: "600px",
-        }}
-      >
+      <div className={styles.intervalsContainer}>
         {INTERVALS.map((int) => (
           <button
             key={int.value}
             onClick={() => onIntervalSelect(int.value)}
-            style={{
-              padding: "8px",
-              background:
-                selectedInterval === int.value
-                  ? UI_COLORS.primary
-                  : UI_COLORS.grayLight,
-              color: selectedInterval === int.value ? "white" : "black",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-            }}
+            className={`${styles.button} ${selectedInterval === int.value ? styles.buttonActive : ""}`}
           >
             {int.label}
           </button>
